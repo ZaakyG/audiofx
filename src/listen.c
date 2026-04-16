93dcbcc2012986a7e62602f8049f4a60c3bbd400
Isaac Gonzalez <urisac.gc@hotmail.com>
2026-04-15 22:02:09 -0600

    added dir for python utils

--- src/listen.c
+++ src/listen.c
@@ -9,51 +9,6 @@
 #define FRAMES_PER_BUFFER 512 
 #define SAMPLE_RATE 44100
 
-
-// Create db function
-/*
-float db_convert(float rms){
-    float db = 20.0f * log10f(rms + 1e-6f);
-    return db;
-}
-
-float linscale(float x){
-    float intercept = 90;
-    float y = intercept * x - intercept;
-    //printf("Scale conversion: %f\n", y);
-    return y;
-}
-
-// Create function to convert to rms
-void rms_display(const float *in)  {
-    float sum = 0.0f;
-    for( unsigned int i = 0; i < FRAMES_PER_BUFFER; i++){
-        sum += in[i] * in[i];
-    }
-
-    float rms = sqrtf(sum/FRAMES_PER_BUFFER);
-    
-    // Create status bar.
-    // First convert to an scale
-    // Convert to dB
-    float db;
-    float scale = 20; 
-    db = db_convert(rms); 
-
-
-    printf("\rLevel: [");
-    for(int i = 0; i <= scale; i++){
-        if (linscale(i/scale) < db){
-            printf("#");
-        } else {
-            printf("-");
-        }
-    }
-    printf("]  %0.5f   ", db);
-    //printf("\rRMS: %f", rms);
-    fflush(stdout);
-}
-*/
 // Callback function to receive the audio samples 
 static int audio_callback(
     const void *inputBuffer, //where samples will be stored
